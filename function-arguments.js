9ef3a5360c41c941a5a6ea3667eb48b2db09465b
Julia Paulasaari <julia@paulasaari.com>
2019-05-16 22:44:06 +0000

    objects task

---
+++ function-arguments.js
@@ -0,0 +1,5 @@
+function math (ArgOne, ArgTwo, ArgThree) {
+  return (ArgTwo * ArgThree) + ArgOne
+}
+
+console.log(math(53, 61, 67));
---
+++ objects.js
@@ -0,0 +1,10 @@
+var pizza = {
+       toppings: ['cheese', 'sauce', 'pepperoni'],
+       crust: 'deep dish',
+       serves: 2
+     };
+  var object = {
+    pizza
+  };
+
+     console.log(pizza);
